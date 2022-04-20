102b36c0d2c36a730d0f72e4c10800e0e2fead32
Janson Xavier <jansonxavier15@gmail.com>
2022-04-20 17:56:38 -0400

    working on 3d ball

--- src/client/components/Message.jsx
+++ src/client/components/Message.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback, Suspense } from "react"
+import { Canvas } from "@react-three/fiber"
+import { useGLTF, useTexture, Shadow, meshBounds } from "@react-three/drei"
+
 // single message
 
 export default ({isSender, msg, username}) => {
 
-  const ball = <div className={`ball${isSender}`}>Hello</div>
 
   return (
       
     <li className={`message-li ${isSender}`}>
       {/* <img className="img-user-message" /> */}
       { username ? <h3 className={`message-username username${isSender}`}>{username}</h3> : null }
-      { username ? ball : null}
+      { username ? <CanvasContainer isSender={isSender}/> : null}
       <h5 className={`message message${username ? 'Yes' : 'No'}${isSender}`}>{msg}</h5>
     </li>
   );
 };
+
+const CanvasContainer = (props) =>{
+  return(
+    <Canvas className={`ball${props.isSender}`}>
+      <Suspense fallback={null}>
+        <Ball />
+      </Suspense>
+    </Canvas>
+
+  )
+}
+
+
+const Ball = () =>{
+  const texture = useTexture("/ball8.jpg")
+  return(
+    
+    <mesh>
+    <sphereGeometry args={[0.1, 64, 64]} />
+      <meshStandardMaterial color="#a6fd29" materialclearcoatRoughness={.6} sheen={1} clearcoat={1} roughness={0.1} map={texture} />
+    </mesh>
+
+  )
+}
